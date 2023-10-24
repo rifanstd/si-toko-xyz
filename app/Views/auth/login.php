d9b3fa578b7c42ceb3dff4ced15bd5fd14df5153
Rifan Setiadi <rfan2442@gmail.com>
2023-10-24 20:44:39 +0700

    Add Admin LTE + Create Halaman Login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -2,39 +2,69 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
         <?= $title; ?>
     </title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/assets/admin-lte/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="/assets/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/assets/admin-lte/dist/css/adminlte.min.css">
 </head>
 
-<body>
-    <h1>Login</h1>
-
-    <form action="login_attempt" method="post">
-        <?php if (session()->getFlashdata('message')): ?>
-            <div class="alert alert-warning">
-                <?= session()->getFlashdata('message') ?>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <b>Sistem Penjualan <br> Toko XYZ</b>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Silahkan Masuk Terlebih Dahulu</p>
+                <form action="login_attempt" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="username" placeholder="Username">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span><i class="fas fa-user"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- /.col -->
+                        <div class="col">
+                            <button type="submit" class="btn btn-primary btn-block">Masuk</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
             </div>
-        <?php endif; ?>
-        <label for="">Username : </label>
-        <input type="text" name="username" id="">
-        <br>
-        <label for="">Password : </label>
-        <input type="password" name="password" id="">
-        <br>
-        <input type="submit" value="Log in">
-    </form>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <!-- jQuery -->
+    <script src="/assets/admin-lte/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="/assets/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="/assets/admin-lte/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
